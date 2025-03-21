77e1f1213817dc5bdfa5686a78e5aa0aa799b7e7
PonizhenDoRyadovogo <art25ivan@gmail.com>
2025-03-21 13:19:36 +0700

    Небольшое изменение: добавлен пробел между class и названием класса

--- abstractFabric/src/ClassUnit.cpp
+++ abstractFabric/src/ClassUnit.cpp
@@ -21,7 +21,7 @@ void ClassUnit::add(const std::shared_ptr<Unit>& unit, Unit::Flags flags)
 
 std::string ClassUnit::compile(unsigned int level) const
 {
-    std::string result = generateShift(level) + "class" + m_name + " {\n";
+    std::string result = generateShift(level) + "class " + m_name + " {\n";
     for(size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i) {
         if(m_fields[i].empty()) {
             continue;
